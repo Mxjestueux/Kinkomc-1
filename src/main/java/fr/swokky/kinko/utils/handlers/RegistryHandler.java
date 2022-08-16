9e43d3edb11565979ecc7307532583e04c485771
cubotique <rpot02@orange.fr>
2022-08-16 19:13:31 +0200

    Initial commit

--- src/main/java/fr/swokky/kinko/utils/handlers/RegistryHandler.java
+++ src/main/java/fr/swokky/kinko/utils/handlers/RegistryHandler.java
@@ -1,7 +1,9 @@
 package fr.swokky.kinko.utils.handlers;
 
 import fr.swokky.kinko.init.ItemInit;
+import fr.swokky.kinko.utils.interfaces.IHasModel;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,6 +14,19 @@ public class RegistryHandler {
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event){
         event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event)
+    {
+        for(Item item : ItemInit.ITEMS)
+        {
+            if(item instanceof IHasModel)
+            {
+                ((IHasModel)item).registerModels();
+            }
+        }
+
     }
 
     public static void preInitRegistries()
